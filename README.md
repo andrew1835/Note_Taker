74cd7280ec20d8ff7c2bfe02b45a313da47f632f
Andrew Anthony <a.anthony1835@gmail.com>
2021-02-28 19:52:41 -0800

    forgot to commit while I was working on it. Have the server set up with the correct routing. Just finishing up Readme now

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Note_Taker
+
+## What, Why, and HOW
+With this assignment, I wanted to use my newfound skills with express. So far, I've really struggled with backend. 
+To achieve the end result, I:
+1. Created a new react app 
+2. Pseudo coded so that I knew how to set up my files/folders
+3. Created all the correct components and pages and linked them up
+4. Put it all together in the PorfolioContainer file, which was exoported to the App.js file, which is then displayed by virtue of the index.js file
+
+The assignment was really valuable. While it's definitely possible to create an awesome portfolio with just HTML and CSS, using React not only strengthened my React skills, but also opened up a lot of opportunities for functionality when it comes to adding components to my portfolio going forward. 
+
+# Links
+Link to GitHub repo: https://github.com/andrew1835/React-Portfolio
+<br>
+Link to deployed application: https://andrewreactportfolio.herokuapp.com/
+
+# Screenshot
+This screenshot shows what the home page of the portfolio looks like
+
+<img src = "./src/components/Images/ReactApp.jpg" alt = "Home page">
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+// express function and port
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+// data pasing, public folder connection
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// HTML routes
+// returns index.html
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+// returns notes.html
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// API routes
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err === true) {
+      console.log(err);
+    } else {
+      const parseData = JSON.parse(data);
+      console.log(parseData);
+      res.send(parseData);
+    }
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const note = req.body;
+  note.id = uuidv4();
+  console.log(note);
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err === true) {
+      console.log(err);
+    } else {
+      console.log("Note recorded");
+
+      const objectData = JSON.parse(data);
+      const allNotes = [...objectData, note];
+      const userData = JSON.stringify(allNotes);
+      res.send(userData);
+      fs.writeFile(path.join(__dirname, "db/db.json"), userData, (err, res) => {
+        if (err === true) {
+          console.log(err);
+        } else {
+          console.log("writing");
+        }
+      });
+    }
+  });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const parameterId = req.params.id;
+  fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("no error");
+    }
+    const newData = JSON.parse(data);
+    for (let i = 0; i < newData.length; i++) {
+      if (newData[i].id === parameterId) {
+        console.log(newData[i]);
+        newData.splice(i, 1);
+
+        const finalData = JSON.stringify(newData);
+        fs.writeFile(
+          path.join(__dirname, "db/db.json"),
+          finalData,
+          (err, data) => {
+            if (err === true) {
+              console.log(err);
+            } else {
+              console.log("no error");
+              res.send("done");
+            }
+          }
+        );
+      }
+    }
+  });
+});
+
+// listen function
+app.listen(PORT, function () {
+  console.log("http://localhost:" + PORT);
+});
